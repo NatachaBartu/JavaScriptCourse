f92d13e647f7809d942ab8798bda4245fba65da0
natacha <nat_bartu@hotmail.com>
2024-02-21 17:19:03 +1000

    exercise some debbuging situations

--- exercises/16-Debugging/debugging-START.js
+++ exercises/16-Debugging/debugging-START.js
@@ -1,12 +1,14 @@
 const people = [
-  { name: 'Wes', cool: true, country: 'Canada' },
-  { name: 'Scott', cool: true, country: 'Merica' },
-  { name: 'Snickers', cool: false, country: 'Dog Country' },
+  { name: "Wes", cool: true, country: "Canada" },
+  { name: "Scott", cool: true, country: "Merica" },
+  { name: "Snickers", cool: false, country: "Dog Country" },
 ];
 
 people.forEach((person, index) => {
   console.log(person.name);
 });
+
+// console.table(people);
 
 // Console Methods
 
@@ -25,6 +27,7 @@ people.forEach((person, index) => {
 // Some Setup Code
 
 function doctorize(name) {
+  console.count("running Doctorize");
   return `Dr. ${name}`;
 }
 
@@ -34,12 +37,12 @@ function greet(name) {
 }
 
 function go() {
-  const name = doctorize(greet('Wes'));
+  const name = doctorize(greet("Wes"));
   console.log(name);
 }
 
-const button = document.querySelector('.bigger');
-button.addEventListener('click', function(e) {
+const button = document.querySelector(".bigger");
+button.addEventListener("click", function (e) {
   const newFontSize =
     parseFloat(getComputedStyle(e.currentTarget).fontSize) + 1;
   e.currentTarget.style.fontSize = `${newFontSize}px`;
@@ -47,9 +50,9 @@ button.addEventListener('click', function(e) {
 
 // A Dad joke fetch
 async function fetchDadJoke() {
-  const res = await fetch('https://icanhazdadjoke.com/', {
+  const res = await fetch("https://icanhazdadjoke.com/", {
     headers: {
-      Accept: 'text/plain',
+      Accept: "text/plain",
     },
   });
   const joke = await res.text();
