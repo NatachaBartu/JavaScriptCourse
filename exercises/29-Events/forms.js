513b1418f5aeffab351f51be77b4d3612fef385f
natacha <nat_bartu@hotmail.com>
2024-02-26 11:04:53 +1000

    Prevent Default and Form Events lesson

---
+++ exercises/29-Events/forms.js
@@ -0,0 +1,34 @@
+const natacha = document.querySelector(".bartu");
+natacha.addEventListener("click", function (e) {
+  const shouldChangePage = confirm(
+    "This  website might be malicious!, do you whish to preceed?"
+  );
+  if (!shouldChangePage) {
+    e.preventDefault();
+  }
+});
+
+const signupForm = document.querySelector(`[name="signup"]`);
+
+signupForm.addEventListener("submit", function (e) {
+  // console.log(e.currentTarget.name.value);
+  // console.log(e.currentTarget.email.value);
+  const name = e.currentTarget.name.value;
+  if (name.includes("chad")) {
+    alert("sorry, name not found!");
+    e.preventDefault();
+  }
+});
+
+function logEvent(e) {
+  console.log(e.type);
+  console.log(e.currentTarget.value);
+}
+signupForm.name.addEventListener("keyup", logEvent);
+signupForm.name.addEventListener("keydown", logEvent);
+signupForm.name.addEventListener("blur", logEvent);
+signupForm.name.addEventListener("focus", logEvent);
+// keyup
+// keydown
+// focus
+// blur
