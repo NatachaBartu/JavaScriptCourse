ce64829f0e62bd8f984691bd39c46c711add191b
natacha <nat_bartu@hotmail.com>
2024-02-26 11:40:30 +1000

    add position where the game will start

--- exercises/33-Etch-a-Sketch/etch-a-sketch.js
+++ exercises/33-Etch-a-Sketch/etch-a-sketch.js
@@ -8,3 +8,9 @@ const ctx = canvas.getContext("2d");
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
 ctx.lineWidth = 10;
+
+//start drawing
+ctx.beginPath();
+ctx.moveTo(200, 200);
+ctx.lineTo(200, 200);
+ctx.stroke();
