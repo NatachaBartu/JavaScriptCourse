e4f701a1c1f6c0b6a4273d9c3b40d34b93ac7ef7
natacha <nat_bartu@hotmail.com>
2024-02-26 11:56:27 +1000

    add a random function for positioning the starting point

--- exercises/33-Etch-a-Sketch/etch-a-sketch.js
+++ exercises/33-Etch-a-Sketch/etch-a-sketch.js
@@ -2,6 +2,8 @@
 const canvas = document.getElementById("#etch-a-sketch");
 const shakeBtn = document.getElementsByClassName(".shake");
 
+console.log(canvas);
+
 const ctx = canvas.getContext("2d");
 
 //set canvas for drawing
@@ -9,8 +11,14 @@ ctx.lineJoin = "round";
 ctx.lineCap = "round";
 ctx.lineWidth = 10;
 
+//randomize position x and y starting points on canvas
+const { width, height } = canvas;
+
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
+
 //start drawing
 ctx.beginPath();
-ctx.moveTo(200, 200);
-ctx.lineTo(200, 200);
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
 ctx.stroke();
