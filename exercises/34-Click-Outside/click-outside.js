ce4f449609c1d874b7e7a7c30e2ad41f91dd581f
natacha <nat_bartu@hotmail.com>
2024-02-29 10:53:09 +1000

    move the constants up to the top, fix typo on modal constants, fix info display for inside of the modal and css for image of modal

--- exercises/34-Click-Outside/click-outside.js
+++ exercises/34-Click-Outside/click-outside.js
@@ -1,10 +1,10 @@
 const cardBtn = document.querySelectorAll(".card button");
+const modalInner = document.querySelector(".modal-inner");
+const modalOuter = document.querySelector(".modal-outer");
 
 function handleCardBtnClick(event) {
   const button = event.currentTarget;
   const card = button.closest(".card");
-  const modalInner = document.querySelector(".modal-inner");
-  modalOuter = document.querySelector(".modal-outer");
 
   //grab image information
   const imgSrc = card.querySelector("img").src;
@@ -13,8 +13,11 @@ function handleCardBtnClick(event) {
 
   //populate modal with new info
   modalInner.innerHTML = `
-    <img src="${imgSrc.replace("200", "600")}" alt="${name}"/>
-    <p>${desc}</p>
+  <img width="600" height="600" src="${imgSrc.replace(
+    "200",
+    "600"
+  )}" alt="${name}"/>
+  <p>${desc}</p>
   `;
 
   //show modal
@@ -24,3 +27,20 @@ function handleCardBtnClick(event) {
 cardBtn.forEach((button) =>
   button.addEventListener("click", handleCardBtnClick)
 );
+
+function closeModal() {
+  modalOuter.classList.remove("open");
+}
+
+modalOuter.addEventListener("click", function (event) {
+  const isOutside = !event.target.closest(".modal-inner");
+  if (isOutside) {
+    closeModal();
+  }
+});
+
+window.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    closeModal();
+  }
+});
