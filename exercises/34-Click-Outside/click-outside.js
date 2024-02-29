6c12d7be9cb12083e32a6c025b86c5a3124ce75e
natacha <nat_bartu@hotmail.com>
2024-02-29 10:32:24 +1000

    grab the card btn and create a function to handle every clicked event, grab the info cards img to display inside of the modal and display it

--- exercises/34-Click-Outside/click-outside.js
+++ exercises/34-Click-Outside/click-outside.js
@@ -1,1 +1,26 @@
-console.log("Testing");
+const cardBtn = document.querySelectorAll(".card button");
+
+function handleCardBtnClick(event) {
+  const button = event.currentTarget;
+  const card = button.closest(".card");
+  const modalInner = document.querySelector(".modal-inner");
+  modalOuter = document.querySelector(".modal-outer");
+
+  //grab image information
+  const imgSrc = card.querySelector("img").src;
+  const desc = card.dataset.description;
+  const name = card.querySelector("h2").textContent;
+
+  //populate modal with new info
+  modalInner.innerHTML = `
+    <img src="${imgSrc.replace("200", "600")}" alt="${name}"/>
+    <p>${desc}</p>
+  `;
+
+  //show modal
+  modalOuter.classList.add("open");
+}
+
+cardBtn.forEach((button) =>
+  button.addEventListener("click", handleCardBtnClick)
+);
