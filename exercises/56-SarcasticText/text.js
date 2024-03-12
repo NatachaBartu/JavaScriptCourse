ac2e87e76784a4298e8f530b2db1a4e07129fd56
natacha <nat_bartu@hotmail.com>
2024-03-12 10:29:29 +1000

    add funky functionality with condition statement

--- exercises/56-SarcasticText/text.js
+++ exercises/56-SarcasticText/text.js
@@ -2,6 +2,76 @@ const textArea = document.querySelector('[name="text"]');
 const result = document.querySelector(".result");
 const filterInputs = Array.from(document.querySelectorAll('[name="filter"]'));
 
+const funkyLetters = {
+  "-": "₋",
+  "!": "ᵎ",
+  "?": "ˀ",
+  "(": "⁽",
+  ")": "₎",
+  "+": "⁺",
+  "=": "₌",
+  0: "⁰",
+  1: "₁",
+  2: "²",
+  4: "₄",
+  5: "₅",
+  6: "₆",
+  7: "⁷",
+  8: "⁸",
+  9: "⁹",
+  a: "ᵃ",
+  A: "ᴬ",
+  B: "ᴮ",
+  b: "ᵦ",
+  C: "𝒸",
+  d: "ᵈ",
+  D: "ᴰ",
+  e: "ₑ",
+  E: "ᴱ",
+  f: "𝒻",
+  F: "ᶠ",
+  g: "ᵍ",
+  G: "ᴳ",
+  h: "ʰ",
+  H: "ₕ",
+  I: "ᵢ",
+  i: "ᵢ",
+  j: "ʲ",
+  J: "ᴶ",
+  K: "ₖ",
+  k: "ₖ",
+  l: "ˡ",
+  L: "ᴸ",
+  m: "ᵐ",
+  M: "ₘ",
+  n: "ₙ",
+  N: "ᴺ",
+  o: "ᵒ",
+  O: "ᴼ",
+  p: "ᵖ",
+  P: "ᴾ",
+  Q: "ᵠ",
+  q: "ᑫ",
+  r: "ʳ",
+  R: "ᵣ",
+  S: "ˢ",
+  s: "ˢ",
+  t: "ᵗ",
+  T: "ₜ",
+  u: "ᵘ",
+  U: "ᵤ",
+  v: "ᵛ",
+  V: "ᵥ",
+  w: "𝓌",
+  W: "ʷ",
+  x: "ˣ",
+  X: "ˣ",
+  y: "y",
+  Y: "Y",
+  z: "𝓏",
+  Z: "ᶻ",
+};
+
 const filters = {
   sarcastic: function (letter, index) {
     //if is odd number will return trufhy
@@ -10,16 +80,23 @@ const filters = {
     }
     return letter.toLowerCase();
   },
-  funky: function () {},
+  funky: function (letter) {
+    // first check if there is a funky letter for this case
+    let funkyLetter = funkyLetters[letter];
+    if (funkyLetter) return funkyLetter;
+    // if there is not, check if there is a lowercase version
+    funkyLetter = funkyLetters[letter.toLowerCase()];
+    if (funkyLetter) return funkyLetter;
+    // if there is nothing, return the regular letter
+    return letter;
+  },
   unable: function () {},
 };
 
 function transformText(text) {
   const filter = filterInputs.find((input) => input.checked).value;
-  console.log(filter);
   //greab the text and loop over each letter
   const modified = Array.from(text).map(filters.sarcastic);
-  console.log(modified);
   result.textContent = modified.join("");
 }
 
