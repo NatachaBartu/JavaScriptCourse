b0e53872cb4a712bfb849abc320d233a7ca586ac
natacha <nat_bartu@hotmail.com>
2024-03-12 10:18:15 +1000

    add sarcastic functionality turn the letter into lower case and upper case

--- exercises/56-SarcasticText/text.js
+++ exercises/56-SarcasticText/text.js
@@ -1,10 +1,26 @@
 const textArea = document.querySelector('[name="text"]');
 const result = document.querySelector(".result");
-const filterInputs = document.querySelector('[name="filter"]');
+const filterInputs = Array.from(document.querySelectorAll('[name="filter"]'));
+
+const filters = {
+  sarcastic: function (letter, index) {
+    //if is odd number will return trufhy
+    if (index % 2) {
+      return letter.toUpperCase();
+    }
+    return letter.toLowerCase();
+  },
+  funky: function () {},
+  unable: function () {},
+};
 
 function transformText(text) {
-  console.log(text);
-  result.textContent = text;
+  const filter = filterInputs.find((input) => input.checked).value;
+  console.log(filter);
+  //greab the text and loop over each letter
+  const modified = Array.from(text).map(filters.sarcastic);
+  console.log(modified);
+  result.textContent = modified.join("");
 }
 
 textArea.addEventListener("input", (e) => transformText(e.target.value));
